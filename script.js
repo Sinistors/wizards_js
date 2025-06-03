1e6c76b430c064dc71c26fbdc7e75b92b6704da5
Nicolas P <nicolas.puissant.pro@gmail.com>
2025-06-03 09:24:17 +0200

    ajout heal et crit

--- script.js
+++ script.js
@@ -17,8 +17,11 @@ class Wizard
     hp;
     strength;
     randomPercent;
+    critChance;
+    healAmount;
+    isHealed = false;
 
-    constructor(playerId, minDamage, maxDamage) 
+    constructor(playerId) 
     {
         this.name = document.querySelector("#" + playerId + " input[data-type='name']").value;
         this.house = document.querySelector("#" + playerId + " select[data-type='house']").value;
@@ -26,19 +29,36 @@ class Wizard
         this.hp = this.hpMax;
         this.strength = parseFloat(document.querySelector("#" + playerId + " input[data-type='strength']").value);
         this.randomPercent = parseFloat(document.querySelector("#" + playerId + " input[data-type='strength']").value);
+        this.critChance = parseFloat(document.querySelector("#" + playerId + " input[data-type='crit']").value);
+        this.healAmount = parseFloat(document.querySelector("#" + playerId + " input[data-type='heal']").value);
     }
 
     attack(wizard) 
     {
-        console.log(this.strength);
+        let atkStr = this.name + " attaque ! ";
         let damage = randomize(this.strength, this.randomPercent);
         wizard.hp -= damage;
+        if (randomRange(0,100) < this.critChance)
+        {
+            damage *= 2;
+            atkStr += "COUP CRITIQUE ! ";
+        }
+
+        atkStr += wizard.name + " a pris " + damage.toFixed(2) + " dégats ! ";
         if (wizard.hp < 0)
         {
             wizard.hp = 0;
         }
+        atkStr += "Il lui reste " + wizard.hp.toFixed(2) + " PV";
 
-        return this.name + " attaque ! " + wizard.name + " a pris " + damage.toFixed(2) + " dégats ! Il lui en reste " + wizard.hp.toFixed(2) + " PV";
+        return atkStr;
+    }
+
+    heal()
+    {
+        this.hp += this.healAmount;
+        this.isHealed = true;
+        return "<span class='text-green'>" + this.name + " se soigne de " + this.healAmount + " PV. Il a maintenant " + this.hp.toFixed(2) + " PV</span>"; 
     }
 
 
@@ -57,21 +77,37 @@ document.querySelector("#startBtn").addEventListener("click", async () =>
 
     fight.innerHTML += "<h2>Historique du duel :</h2>";
 
+    let historicDiv = document.createElement("div");
+    fight.append(historicDiv);
+
     let turn = 0;
     let random = randomRange(1,2)-1;
     while (wizard1.hp > 0 && wizard2.hp > 0) 
     {
-        //console.log(randomize(10, 5));
         let historic = document.createElement("p");
         if ((turn + random) % 2 == 0) 
         {
-            historic.innerText = wizard1.attack(wizard2);
+            if ((wizard1.hp / wizard1.hpMax) < 0.5 && !wizard1.isHealed)
+            {
+                historic.innerHTML = wizard1.heal();
+            }
+            else
+            {
+                historic.innerHTML = wizard1.attack(wizard2);
+            }
         } 
         else 
         {
-            historic.innerText = wizard2.attack(wizard1);
+            if ((wizard2.hp / wizard2.hpMax) < 0.5 && !wizard2.isHealed)
+            {
+                historic.innerHTML = wizard2.heal();
+            }
+            else
+            {
+                historic.innerHTML = wizard2.attack(wizard1);
+            }
         }
-        fight.append(historic);
+        historicDiv.prepend(historic);
 
         document.querySelector("#hpBar1 div").style.width = wizard1.hp / wizard1.hpMax * 100 + "%";
         document.querySelector("#hpBar2 div").style.width = wizard2.hp / wizard2.hpMax * 100 + "%";
@@ -93,5 +129,5 @@ document.querySelector("#startBtn").addEventListener("click", async () =>
     }
     const resultElement = document.createElement("p");
     resultElement.innerText = deadPlayerName + " est mort !";
-    fight.append(resultElement);
+    historicDiv.prepend(resultElement);
 });
